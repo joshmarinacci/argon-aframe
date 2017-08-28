62511658a51f35a8ba7fe069885e597758536170
josh <joshua@marinacci.org>
2017-08-28 13:42:57 -0700

    update samples to use the new build system switch to webpack to import threejs and aframe into the build import ArgonSunMoon and CSS3DArgon* into the build create buildit script to build the examples site instead of using Jekyll add build-lip and built-site npm run targets add dep for threejs directly update to newer webpack

---
+++ tools/buildit.js
@@ -0,0 +1,67 @@
+console.log('building');
+
+var FS = require('fs');
+var PATHS = require('path');
+
+const OUTDIR = "dist";
+mkdir(OUTDIR);//if(!FS.existsSync(OUTDIR)) FS.mkdirSync(OUTDIR);
+const EXAMPLES_OUTDIR = PATHS.join(OUTDIR,'examples');
+mkdir(EXAMPLES_OUTDIR);//if(!FS.existsSync(EXAMPLES_OUTDIR)) FS.mkdirSync(EXAMPLES_OUTDIR);
+
+const config = {
+    keywords:    "argon, aframe, augmented reality, web, javascript",
+    argonaframejs: "../../argon-aframe.js",
+    samples: "https://samples.argonjs.io",
+};
+
+function mkdir(dir) {
+    if(!FS.existsSync(dir)) FS.mkdirSync(dir);
+}
+copyDirRecursive("examples","dist/examples").then(()=>{
+    console.log("fully done with copying");
+    processPage("examples/basic/index.html");
+    processPage("examples/geoposition/index.html");
+    processPage("examples/panorama/index.html");
+});
+
+function copyDirRecursive(src,dst) {
+    mkdir(dst);
+    return Promise.all(FS.readdirSync(src).map((file)=>{
+        const fpath = PATHS.join(src,file);
+        const dpath = PATHS.join(dst,file);
+        if(FS.lstatSync(fpath).isDirectory()) {
+            return copyDirRecursive(fpath,dpath);
+        } else {
+            return copyFile(fpath,dpath);
+        }
+    }));
+}
+
+function copyFile(fpath,dpath) {
+    return new Promise((res,rej)=>{
+        const reader = FS.createReadStream(fpath);
+        const writer = FS.createWriteStream(dpath);
+        writer.on('finish',()=> res());
+        reader.pipe(writer);
+    });
+}
+
+function processPage(path) {
+    const parsed = PATHS.parse(path);
+    console.log(PATHS.parse(path));
+
+    var file = FS.readFileSync(path).toString();
+    file = file.replace(/{{ site.keywords }}/g, config.keywords);
+    file = file.replace(/{{ site.argonaframejs }}/g, config.argonaframejs);
+    file = file.replace(/{{ site.samples }}/g,config.samples);
+
+
+
+    const fdir = PATHS.join(OUTDIR,parsed.dir);
+    if (!FS.existsSync(fdir)) FS.mkdirSync(fdir);
+    const fpath = PATHS.join(OUTDIR,parsed.dir,parsed.base);
+    console.log(fpath);
+
+    FS.writeFileSync(fpath, file);
+}
+
