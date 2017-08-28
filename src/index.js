5c9a2491e6b796ca360d3225ea457cd2a2ad42f5
josh <joshua@marinacci.org>
2017-08-28 13:42:57 -0700

    update samples to use the new build system switch to webpack to import threejs and aframe into the build import ArgonSunMoon and CSS3DArgon* into the build create buildit script to build the examples site instead of using Jekyll add build-lip and built-site npm run targets add dep for threejs directly update to newer webpack
    # Conflicts:
    #	package.json
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import * as THREE from "three";
+console.log("THREE is",THREE);
+import * as AFRAME from "aframe";
+console.log("AFRAME is", AFRAME);
 require('../src/ar-scene.js');
 require('../src/ar-components.js');
 require('../src/ar-referenceframe.js');
@@ -5,3 +9,7 @@ require('../src/css-object.js');
 require('../src/ar-vuforia.js');
 require('../src/ar-jsartoolkit.js');
 require('../src/panorama-reality.js');
+
+import "./ArgonSunMoon";
+import "./CSS3DArgonHUD";
+import "./CSS3DArgonRenderer";
